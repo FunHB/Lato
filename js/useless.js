f6a458ef1d32ff4d66cba303bfa53a080f369f97
FunHB <marcel.krolczyk@gmail.com>
2020-04-17 19:07:17 +0200

    [updated]

--- js/useless.js
+++ js/useless.js
@@ -4,7 +4,7 @@ var hearts = document.getElementsByClassName("fa-heart");
 function active(i) {
 
     if (hearts[i].classList.contains("heart-active")) {
-        heart[i].classList.remove("heart-active");
+        hearts[i].classList.remove("heart-active");
     } else {
         hearts[i].classList.add("heart-active");
     }
